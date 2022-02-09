de473f0a996258fbb232c99b699a3036ee67ecc5
Smitfall <captainfalconrsmith@gmail.com>
2022-02-09 08:08:27 -0700

    Add project files.

---
+++ MultiplicationTable/MultiplicationTable.vb
@@ -0,0 +1,40 @@
+﻿'Fallon Smith
+'RCET0265
+
+Option Explicit On
+Option Strict On
+
+Module MultiplicationTable
+
+    Sub Main()
+
+        Dim tableDimensions As Integer
+        Dim wholeNumber As Boolean
+
+        Do While wholeNumber = False
+            Try
+                Console.WriteLine("Please type a number.")
+                tableDimensions = CInt(Console.ReadLine())
+                wholeNumber = True
+            Catch
+                wholeNumber = False
+            End Try
+        Loop
+
+
+        Console.WriteLine("Enjoy your " & tableDimensions & " X " & tableDimensions &
+                                  " multiplication table")
+
+        For row = 1 To tableDimensions
+            For col = 1 To tableDimensions
+                Console.Write(Str(col * row).PadLeft(5))
+
+            Next
+            Console.WriteLine()
+        Next
+
+        Console.Read()
+    End Sub
+
+
+End Module
